d4a0a3370e3061efda43ffb9573fbcb927c79dc4
Letlhogonolo Sekwati <letlhogonolo.philippi@thedigitalacademy.co.za>
2021-09-10 16:10:17 +0200

    layout changed

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
-import {ProductContainer,Breadcrumbs,BreadcrumbsList,ProductBox,ProductImgs,ProductMainImg,ProductFullInfo} from "./styles"
+import React from "react";
+import {
+  ProductContainer,
+  Breadcrumbs,
+  BreadcrumbsList,
+  ProductBox,
+  ProductImgContainer,
+  ProductImgs,
+  ProductMainContainerImg,
+  ProductMainImg,
+  Image,
+  ProductFullInfo,
+} from "./styles";
 
 const Product = () => {
-    return (
-        <ProductContainer>
-            <Breadcrumbs>
-                <BreadcrumbsList>Brands Name</BreadcrumbsList>
-                <BreadcrumbsList> Product Name</BreadcrumbsList>
-            </Breadcrumbs>
+  return (
+    <ProductContainer>
+      <Breadcrumbs>
+        <BreadcrumbsList>Brands Name</BreadcrumbsList>
+        <BreadcrumbsList> Product Name</BreadcrumbsList>
+      </Breadcrumbs>
 
-            <ProductBox>
+      <ProductBox>
+        <ProductImgContainer>
+          <ProductMainContainerImg>
+            <ProductMainImg>
+                <Image src="../products/shoes.jpg" alt="" />
+            </ProductMainImg>
+          </ProductMainContainerImg>
 
-                <ProductImgs></ProductImgs>
-                <ProductMainImg></ProductMainImg>
-                <ProductFullInfo></ProductFullInfo>                
-            </ProductBox>
-        </ProductContainer>
-    )
-}
+          <ProductImgs>
+            <h5>small pics</h5>
+            <h5>small pics</h5>
+            <h5>small pics</h5>
+          </ProductImgs>
+        </ProductImgContainer>
+        <ProductFullInfo>
+          <h4>product details</h4>
+        </ProductFullInfo>
+      </ProductBox>
+    </ProductContainer>
+  );
+};
 
-export default Product
+export default Product;
--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -11,6 +11,7 @@ background-color: black;
 export const Breadcrumbs = styled.ul`
   padding: 10px 16px;
   list-style: none;
+  display:flex;
   background-color: #eee;
 
 `;
@@ -27,30 +28,59 @@ export const BreadcrumbsList = styled.li`
     }
 `;
 
+
 export const ProductBox = styled.div`
 
     height:100vh;
- 
     background-color:yellow;
     display:flex;
-`;
+    width:100%;
+    position:sticky;
+`
+
+export const ProductImgContainer = styled.div`
+
+display:flex;
+height:100vh;
+flex-direction:column;
+flex:0.4;
+`
+
 
 export const ProductImgs = styled.div`
-
 height:100vh;
- 
 background-color:green;
+display:flex;
+align-items:center;
+justify-content:center;
+
+&h5{
+  border-top:1px solid red;
+  height:10%;
+  width:10%;
+
+} 
 
 `;
+export const ProductMainContainerImg = styled.div`
+height:100vh;
+background-color:blue;
+`
 
 export const ProductMainImg = styled.div`
 
-height:100vh;
- 
-background-color:blue;
+  height:50%;
+  width:50%;
 `;
+
+export const Image = styled.img`
+height:100%;width:100%;
+object-fit:cover;
+
+`
 
 export const ProductFullInfo = styled.div`
 
 background-color:red;
+flex:0.6;
 `;
